e56c94bc3918d8752e7a2526d8fc502ac4ced932
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-01-01 00:09:24 +0900

    feat: update instrument phrase handling

--- Meowziq/Core/Phrase.cs
+++ Meowziq/Core/Phrase.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Meowziq.Core {
     /// <summary>
@@ -44,6 +45,63 @@ namespace Meowziq.Core {
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // protected Methods [verb]
+
+        protected void applyMonoNote(int position, int beatCount, Key key, List<Span> spanList, string target, int interval = 0) {
+            int _index = 0; // 16beatのindex
+            int _indexCount = 0; // 16beatで1拍をカウントする用
+            int _spanIndex = 0; // Span リストの添え字
+            var _valueArray = filter(target).ToCharArray();
+            foreach (var _value in _valueArray) {
+                if (_index > beatCount * 4) {
+                    return; // Pattern の長さを超えたら終了 FIXME: 長さに足りない時？ エラー? リピート？
+                }
+                if (_indexCount == 4) { // 16beatが4回進んだ時(1拍)
+                    _indexCount = 0; // カウンタリセット
+                    _spanIndex++; // Span の index値
+                }
+                if (Regex.IsMatch(_value.ToString(), @"^[1-7]+$")) { // 度数の数値がある時
+                    var _span = spanList[_spanIndex];
+                    int _noteNum;
+                    // 曲の旋法と Span の旋法が同じ場合は自動旋法
+                    if (_span.KeyMode == _span.Mode) {
+                        _noteNum = Utils.GetNoteWithAutoMode(key, _span.Degree, _span.KeyMode, int.Parse(_value.ToString()));
+                    }
+                    // Span に旋法が設定してあればそちらを適用する
+                    else {
+                        _noteNum = Utils.GetNoteWithSpanMode(key, _span.Degree, _span.KeyMode, _span.Mode, int.Parse(_value.ToString()));
+                    }
+                    // この音の音価を調査する
+                    int _gateCount = 0;
+                    int _all16beatcount = (beatCount * 4);
+                    for (var _i = _index + 1; _i < _all16beatcount; _i++) {
+                        var _search = _valueArray[_i];
+                        if (_search.Equals('>')) {
+                            _gateCount++; // 16beat分伸ばす
+                        }
+                        if (!_search.Equals('>')) {
+                            break; // '>' が途切れたら終了
+                        }
+                    }
+                    var _gate = 120 * (_gateCount + 1);
+                    Add(new Note(position + (120 * _index), (int) _noteNum + interval, _gate, 127)); // +1 は数値分
+                }
+                _index++; // 16beatを進める
+                _indexCount++; // Span 用のカウンタも進める
+            }
+        }
+
+        protected void applyDrumNote(int position, int beatCount, string target, Percussion noteNum) {
+            int _index = 0;
+            foreach (bool? _value in convertToBool(filter(target))) {
+                if (_index > beatCount * 4) {
+                    return; // Pattern の長さを超えたら終了 FIXME: 長さに足りない時？ エラー? リピート？
+                }
+                if (_value == true) {
+                    Add(new Note(position + (120 * _index), (int) noteNum, 120, 127));
+                }
+                _index++; // 16beatを進める
+            }
+        }
 
         protected static string filter(string target) {
             // 不要文字削除
