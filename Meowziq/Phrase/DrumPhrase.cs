96dfa462fae7feb578c131cc2f99511a27b64376
Hiroyuki Adachi <hiroxpepe@gmail.com>
2020-12-31 19:42:57 +0900

    feat: update drum and bass players

--- Meowziq/Phrase/DrumPhrase.cs
+++ Meowziq/Phrase/DrumPhrase.cs
@@ -1,6 +1,4 @@
 ﻿
-using System.Collections.Generic;
-
 using Meowziq.Core;
 
 namespace Meowziq.Phrase {
--- Meowziq/Player/BassPlayer.cs
+++ Meowziq/Player/BassPlayer.cs
@@ -1,23 +1,15 @@
 ﻿
-using Meowziq.Core;
 using Meowziq.Phrase;
 
 namespace Meowziq.Player {
     public class BassPlayer : Core.Player {
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
-        // Constructor
-
-        public BassPlayer(Song song, int midiCh, int programNum) : base(song, midiCh, programNum) {
-            this.phraseList.Add(new TextBassPhrase());
-        }
-
-        public BassPlayer(Song song, MidiChannel midiCh, Instrument programNum) : base(song, midiCh, programNum) {
-            this.phraseList.Add(new TextBassPhrase());
-        }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////
         // protected Methods [verb]
+
+        override protected void preBuild() {
+            this.phraseList.Add(new TextBassPhrase());
+        }
 
         override protected void onPlay() {
             throw new System.NotImplementedException();
