c2a02fb331ce97bddd1fd0e17e83412ace7442e0
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-01-06 21:08:20 +0900

    feat: update song phrase flow

--- Meowziq/Message.cs
+++ Meowziq/Message.cs
@@ -13,7 +13,7 @@ namespace Meowziq {
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // Fields
 
-        // Tick 毎の メッセージのリスト TODO: チャンネル
+        // Tick 毎の メッセージのリスト
         Dictionary<int, List<ChannelMessage>> item = new Dictionary<int, List<ChannelMessage>>();
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,9 +21,8 @@ namespace Meowziq {
 
         public List<ChannelMessage> GetBy(int tick) {
             tick = tick - 1; // -1 が必要
-            List<ChannelMessage> _returnList;
             if (item.ContainsKey(tick)) { // その tick が存在するかどうか
-                _returnList = item[tick];
+                var _returnList = item[tick];
                 item.Remove(tick); // 重複回避の為、削除
                 return _returnList;
             }
@@ -47,11 +46,11 @@ namespace Meowziq {
 
         void add(int tick, ChannelMessage channelMessage) {
             if (!item.ContainsKey(tick)) {
-                List<ChannelMessage> _newList = new List<ChannelMessage>();
+                var _newList = new List<ChannelMessage>();
                 _newList.Add(channelMessage);
                 item.Add(tick, _newList); // 新規追加
             } else {
-                List<ChannelMessage> _list = item[tick];
+                var _list = item[tick];
                 _list.Add(channelMessage); // 追加
             }
         }
