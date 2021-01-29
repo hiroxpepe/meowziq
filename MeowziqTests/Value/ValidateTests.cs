fc2661045a533974f684378e0eaa5313db785ce8
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-01-29 12:43:30 +0900

    update

---
+++ MeowziqTests/Value/ValidateTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Meowziq.Value;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meowziq.Value.Tests {
+    [TestClass()]
+    public class ValidateTests {
+
+        [TestMethod()]
+        public void PhraseValueTest1() {
+            var result = new PrivateType(typeof(Validate)).InvokeStatic("PhraseValue", "[1111|----|5555|----]"); // 正常データ
+            Assert.AreEqual(result, "[1111|----|5555|----]");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormatException))]
+        public void vPhraseValueTest2() {
+            new PrivateType(typeof(Validate)).InvokeStatic("PhraseValue", "[--1--|----|----|----]"); // 例外をテスト
+        }
+    }
+}
