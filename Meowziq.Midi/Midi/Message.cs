c102ad68471db1a3114c4404a48bbac9ae0609e1
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-10-12 20:57:54 +0900

    refactor: #1 keeping codes clean.

--- Meowziq.Midi/Midi/Message.cs
+++ Meowziq.Midi/Midi/Message.cs
@@ -1,4 +1,18 @@
-﻿
+﻿/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +23,7 @@ using Meowziq.Core; // depends on Meowziq.Core.Note
 namespace Meowziq.Midi {
     /// <summary>
     /// message class using Sanford.Multimedia.Midi
+    /// @author h.adachi
     /// </summary>
     /// <fixme>
     /// relies on abstraction instead of ChannelMessage.
